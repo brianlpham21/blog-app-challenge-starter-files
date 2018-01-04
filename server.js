7f73494c7e06ae1b034321b131763089c1b3b8e5
Brian Pham <brianlpham21@gmail.com>
2018-01-04 15:00:40 -0800

    after npm install

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('common'));
 
 // you need to import `blogPostsRouter` router and route
 // requests to HTTP requests to `/blog-posts` to `blogPostsRouter`
-
+//
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
